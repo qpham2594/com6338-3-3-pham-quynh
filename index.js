9b581b2557e64a79cc4053dc34064ebb3ed19c2c
Quynh <qpham2594@gmail.com>
2023-05-31 16:32:11 -0400

    questionArr setup

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 // Your code here
 
+/* var questionsArr = [
+    {
+        question: 'You are drained after hanging out with people',
+        answer: true,
+        question: 'You feel recharged when you are home',
+        answer: true,
+        question: 'I like to do things by myself',
+        answer: true,
+        question: 'I dont know how people have so many plans',
+        answer: true
+    }
+]
+
+console.log (questionsArr)
+
+function runQuiz () {
+    for (let index = 0; index < array.length; index++) {
+        const questionsArr = questionsArr[i];
+        
+    }
+} */
+
+var questionsArr = [
+    {
+        question:'You are drained after hanging out with people',
+        answer: true },
+    { 
+        question:'You feel recharged when you are home',
+        answer: true },
+    {
+        question: 'I like to do things by myself',
+        answer: true },
+    {
+        question:'I dont know how people have so many plans',
+        answer: true },
+    {
+        question: 'Sometimes I do not go outside for days',
+        answer: true
+    }
+    ]
+
+var correctAns
+function runQuiz () {
+    /*for (let index = 0; index < array.length; index++) {
+        var questions = questionsArr[i]
+        var answer = confirm (questions);
+        if (answer) {
+            correctAns++
+        }
+        
+    }
+
+    if (correctAns >= 2) {
+        alert ("You're in introvert")
+    } else {
+        alert ("You're an extrovert")
+    } */
+}
