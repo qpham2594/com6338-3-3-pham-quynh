da7cab075eb63685c644b3c4e0acfeceec9af93d
Quynh <qpham2594@gmail.com>
2023-06-01 13:35:06 -0400

    update

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
 // Your code here
 
-/* var questionsArr = [
-    {
-        question: 'You are drained after hanging out with people',
-        answer: true,
-        question: 'You feel recharged when you are home',
-        answer: true,
-        question: 'I like to do things by myself',
-        answer: true,
-        question: 'I dont know how people have so many plans',
-        answer: true
-    }
-]
-
-console.log (questionsArr)
-
-function runQuiz () {
-    for (let index = 0; index < array.length; index++) {
-        const questionsArr = questionsArr[i];
-        
-    }
-} */
 
 var questionsArr = [
     {
-        question:'You are drained after hanging out with people',
-        answer: true },
+        question:'An object includes a variables and function',
+        answer: true 
+    },
     { 
-        question:'You feel recharged when you are home',
-        answer: true },
+        question:'There are for loops, while loops, and do...while loops',
+        answer: true 
+    },
     {
-        question: 'I like to do things by myself',
-        answer: true },
+        question: 'For loops are the most commonly used',
+        answer: true
+    },
     {
-        question:'I dont know how people have so many plans',
-        answer: true },
+        question:'If a variable is part of an object, it is called a property',
+        answer: true 
+    },
     {
-        question: 'Sometimes I do not go outside for days',
+        question: 'You can modify an array after it is created',
         answer: true
     }
     ]
 
-var correctAns
-function runQuiz () {
-    /*for (let index = 0; index < array.length; index++) {
-        var questions = questionsArr[i]
+// objects, 5, 
+
+
+
+
+var correctAns = 0
+var totalQuest = 0
+
+function runQuiz() {
+    console.log ('started quiz')
+    for ( var i = 0; i < questionsArr.length; i++) {
+       // correctAns += questionsArr[i][1]
+        //var score = (correctAns/questionsArr.length)
+        // alert ('Your score is' + score)
+        var questions = questionsArr[i];
+        console.log(questions)
         var answer = confirm (questions);
         if (answer) {
             correctAns++
         }
         
     }
-
-    if (correctAns >= 2) {
-        alert ("You're in introvert")
-    } else {
-        alert ("You're an extrovert")
-    } */
+    
+    
+    
 }
+
+//quizScore = Math.round ((correctAns/totalQuest)*100)
+//alert("Your score is" + quizScore + "%" )
+
+//function percentage (partialValue,totalValue) {
+  //  return (100*partialValue)/totalValue
+// }
