c953af11841908426063da83e2b1852d6d6a83f2
Quynh <qpham2594@gmail.com>
2023-06-02 16:27:12 -0400

    final update with comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Your code here
 
+// global variable = questionsArr - each question is a string, and each answer is a boolean, each set of question and answer is an object, so there are 6 objects total, each separated by a comma
 
 var questionsArr = [
 
@@ -29,26 +30,23 @@ var questionsArr = [
     },
     ]
 
+function runQuiz() { // funciton runQuiz will work with the id in HTML page
+var correctAns = 0  // variable correctAns acts as a score counter and it set to equal 0 
 
-function runQuiz() {
-var correctAns = 0
+    console.log ('started quiz') // console log here to make sure it is working when the button is clicked
 
-    console.log ('started quiz')
+    // for loop is used with three statements; statement 1 is the index = 0, statement 2 states the index is to be less than the length of the questionsArr (so 6 questions in this case), and statement 3 states that each time a code block is done, it will go to the next one
     for ( let i = 0; i < questionsArr.length; i++) {
-    //  userResponse = confirm(questionsArr.answer)
 
-        var questions = questionsArr[i];
-        console.log(questionsArr)
-        var answer = confirm (questions.question);
-
-        if (answer === questions.answer) {
+        var questions = questionsArr[i]; //defining variable questions as questionsArr
+        var answer = confirm (questions.question); // defining answer as the confirm popup box specifically with just the question by using dot operator to access the question property, without the dot operator, questions = questionsArr which would pop up both questions and the answers
+        if (answer === questions.answer) { // statement is that if the answer (by user) is the same as the answer from questionsArr (once again using dot operator here), then the correctAns (the score counter that was set at 0 originally) will add 1 
             correctAns++
-        }
-        
+        }    
     }
-    var quizScore = Math.round(correctAns/questionsArr.length * 100) 
-    console.log(quizScore)
-    alert ('Your score is ' + quizScore + '%')
+    
+    var quizScore = Math.round(correctAns/questionsArr.length * 100) // to round a number to the closest interger, use math.round; define quizScore as Math.round with the equation; essentially like y = x
+    alert ('Your score is ' + quizScore + '%') // give alert box with the statement in quotation marks plus the quizScore plus the percentage sign to form a sentence
     
 }
 
